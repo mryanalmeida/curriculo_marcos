7d96240e153ce05dba082c415f9cc84f9867e48c
Marcos Ryan <148161241+mryanalmeida@users.noreply.github.com>
2024-10-01 01:55:16 -0300

    Add files via upload

---
+++ ano.js
@@ -0,0 +1,7 @@
+/**
+ * Atualizar Ano Automaticamente
+ * @author Andrew Dantas
+ */
+let ano = document.getElementById('copyrightYear')
+let anoAtual = new Date().getFullYear()
+ano.textContent = anoAtual
